fe509c80af7c09a6b6856485ca4b82d9fda630ce
vrde <agranzot@gmail.com>
2015-07-31 10:33:49 +0200

    Linting

--- jquery.shmui.js
+++ jquery.shmui.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * Dear friend, thanks for taking a look at the code.
  * It's still refactored, I'll do it when I'm sure this gallery has a decent
@@ -9,14 +11,13 @@
  *  -vrde
  */
 
-(function ($) {
-    'use strict'
+(function($) {
 
     // Keys to trap when plugin is active
     var TRAPPED = ['Esc', 'Left', 'Right', ' '],
         DEVICE_IMAGE_MAX_RATIO = 2;
 
-    $.fn.shmui = function (options) {
+    $.fn.shmui = function () {
 
         var state = {
                 // array containing the galleries
@@ -38,18 +39,42 @@
 
                 // the id for the inertia timer
                 inertia: null
-            },
+            };
 
             // soon...
-            settings = $.extend({
-            }, options);
+            // settings = $.extend({
+            // }, options);
 
-        function getEl () {
+        function change (offset) {
+            var gallery = state.galleries[state.currentGallery],
+                pos = (state.currentImage + offset) % gallery.length;
+
+            if (gallery.length === 1) {
+                return;
+            }
+
+            if (pos === -1) {
+                pos = gallery.length - 1;
+            }
+            state.currentImage = pos;
+
+            update();
+        }
+
+        function toggleZoom (e) {
+            if (state.el.find('.zoom').length !== 0) {
+                unzoom();
+            } else {
+                zoom(e);
+            }
+        }
+
+        function getEl() {
             var content, controls, prev, $zoom, next;
 
             if (!state.el) {
                 state.el = $('<div />', {
-                    'class': 'shmui-wrap',
+                    'class': 'shmui-wrap'
                 });
 
                 content = $('<div />', {
@@ -73,69 +98,58 @@
                 });
 
 
-                if (state.galleries[state.currentGallery].length > 1)
+                if (state.galleries[state.currentGallery].length > 1) {
                     controls.append(prev, $zoom, next);
-                else
+                } else {
                     controls.append($zoom);
+                }
 
                 state.el.append(content);
                 state.el.append(controls);
                 state.el.on('click', function (e) {
-                    if ($(e.target).hasClass('shmui-wrap'))
+                    if ($(e.target).hasClass('shmui-wrap')) {
                         close();
+                    }
                 });
 
-                prev.on('click', function (e) { $(this).blur(); change(-1); });
+                prev.on('click', function () { $(this).blur(); change(-1); });
                 $zoom.on('click', function (e) { $(this).blur(); toggleZoom(e); });
-                next.on('click', function (e) { $(this).blur(); change(1); });
+                next.on('click', function () { $(this).blur(); change(1); });
                 $('body').prepend(state.el).addClass('shmui-stop-scrolling');
             }
             return state.el;
         }
 
-        function getSrc () {
+        function getSrc() {
             var img = state.galleries[state.currentGallery][state.currentImage],
                 url = img.attr('data-large-src') || img.attr('src');
             return url;
         }
 
-        function preloadNextImage () {
+        function preloadNextImage() {
             var gallery = state.galleries[state.currentGallery],
                 pos = (state.currentImage + 1) % gallery.length,
                 imgObj = new Image(),
                 img, url;
 
-            if (pos == -1)
-                pos = gallery.length - 1
+            if (pos === -1) {
+                pos = gallery.length - 1;
+            }
 
-            img = state.galleries[state.currentGallery][pos],
+            img = state.galleries[state.currentGallery][pos];
             url = img.attr('data-large-src') || img.attr('src');
             imgObj.src = url;
             return url;
         }
 
-        function change (offset) {
-            var gallery = state.galleries[state.currentGallery],
-                pos = (state.currentImage + offset) % gallery.length;
-
-            if (gallery.length == 1)
-                return;
-
-            if (pos == -1)
-                pos = gallery.length - 1
-            state.currentImage = pos;
-
-            update();
-        }
-
-        function goto (imageIndex, galleryIndex) {
+        function goto(imageIndex, galleryIndex) {
             state.currentImage = imageIndex;
             state.currentGallery = galleryIndex;
 
             update();
         }
 
-        function update () {
+        function update() {
             var el = getEl(),
                 content = el.find('.shmui-content'),
                 newContent = $('<div />', { 'class': 'shmui-content' }),
@@ -145,7 +159,7 @@
             unzoom();
 
             content.after(newContent);
-            newContent.css('background-image', 'url("'+ url + '")');
+            newContent.css('background-image', 'url("' + url + '")');
 
             newContent.on('click', close);
             el.addClass('loading');
@@ -160,11 +174,11 @@
             i.src = url;
 
             content.fadeOut('fast', function () {
-                content.remove()
+                content.remove();
             });
         }
 
-        function close () {
+        function close() {
             $('.shmui-wrap').remove();
             $('body').removeClass('shmui-stop-scrolling');
             state.el = null;
@@ -172,38 +186,34 @@
             state.currentImage = null;
         }
 
-        function clicked (e) {
+        function clicked() {
             var img = $(this);
-            if (!state.lastLocation)
+            if (!state.lastLocation) {
                 state.lastLocation = window.location.pathname + window.location.search;
+            }
 
             goto(img.data('imageIndex'), img.data('galleryIndex'));
         }
 
-        function toggleZoom (e) {
-            if (state.el.find('.zoom').length != 0)
-                unzoom();
-            else
-                zoom(e);
-        }
 
-        function optimizeImageSize (imageWidth, imageHeight) {
+        function optimizeImageSize(imageWidth, imageHeight) {
             var deviceSize = Math.max($(window).width(), $(window).height()),
                 imageSize = Math.max(imageWidth, imageHeight),
                 maxSize = deviceSize * DEVICE_IMAGE_MAX_RATIO,
                 resizeRatio = maxSize / imageSize;
 
             // do not stretch the image
-            if (resizeRatio > 1)
+            if (resizeRatio > 1) {
                 resizeRatio = 1;
+            }
 
             return {
                 width: imageWidth * resizeRatio,
                 height: imageHeight * resizeRatio
-            }
+            };
         }
 
-        function zoom (e) {
+        function zoom() {
             var el = getEl(),
                 content = el.find('.shmui-content'),
                 img = new Image(),
@@ -214,8 +224,12 @@
             content.on('click', unzoom);
             content.addClass('zoom');
 
-            img.onload = function () {
-                function mouseAdaptor (e) {
+            img.onload = function() {
+                var imageWidth = this.width,
+                    imageHeight = this.height,
+                    resizedImageDimension = optimizeImageSize(imageWidth, imageHeight);
+
+                function mouseAdaptor(e) {
                     var w = el.width(),
                         h = el.height(),
                         x = -e.clientX * (imageWidth - w) / w,
@@ -225,7 +239,7 @@
                     e.preventDefault();
                 }
 
-                function touchAdaptor (e) {
+                function touchAdaptor(e) {
                     var touch = e.originalEvent.touches[0],
                         w = el.width(),
                         h = el.height();
@@ -254,7 +268,7 @@
                     e.preventDefault();
                 }
 
-                function inertiaAdaptor () {
+                function inertiaAdaptor() {
                     var w = el.width(),
                         h = el.height();
 
@@ -278,10 +292,7 @@
                     reposition(lastX, lastY);
                 }
 
-                function reposition (x, y) {
-                    var w = el.width(),
-                        h = el.height();
-
+                function reposition(x, y) {
                     x = x + 'px';
                     y = y + 'px';
 
@@ -299,9 +310,6 @@
                     //content.css('background-position', x + ' ' + y);
                 }
 
-                var imageWidth = this.width,
-                    imageHeight = this.height,
-                    resizedImageDimension = optimizeImageSize(imageWidth, imageHeight);
 
                 imageWidth = resizedImageDimension.width;
                 imageHeight = resizedImageDimension.height;
@@ -324,19 +332,20 @@
                 //    mouseAdaptor(e);
 
                 el.on('mousemove', mouseAdaptor);
-                el.on('touchmove', function (e) {
-                    if($(e.target).hasClass('zoom'))
+                el.on('touchmove', function(e) {
+                    if($(e.target).hasClass('zoom')) {
                         touchAdaptor(e);
-                    else
+                    } else {
                         e.preventDefault();
+                    }
                 });
-                el.on('touchstart', function (e) {
+                el.on('touchstart', function(e) {
                     var t = e.originalEvent.touches[0];
                     clearInterval(state.inertia);
                     lastTouchX = t.clientX;
                     lastTouchY = t.clientY;
                 });
-                el.on('touchend', function (e) {
+                el.on('touchend', function(e) {
                     if($(e.target).hasClass('zoom')) {
                         inertiaAdaptor();
                         state.inertia = setInterval(inertiaAdaptor, 30);
@@ -347,12 +356,12 @@
                     }
                     //e.preventDefault();
                 });
-            }
+            };
 
             img.src = getSrc();
         }
 
-        function unzoom () {
+        function unzoom() {
             var el = getEl(),
                 content = el.find('.shmui-content');
 
@@ -378,32 +387,36 @@
             content.on('click', close);
         }
 
-        function keypress (e) {
+        function keypress(e) {
             var k = e.key;
 
-            if(!state.el)
+            if(!state.el) {
                 return;
+            }
 
-            if (k == 'Esc')
+            if (k === 'Esc') {
                 close();
-            else if (k == 'Left')
+            } else if (k === 'Left') {
                 change(-1);
-            else if (k == 'Right' || k == ' ')
+            } else if (k === 'Right' || k === ' ') {
                 change(1);
-            else if (k == 'z')
+            } else if (k === 'z') {
                 toggleZoom(e);
+            }
 
-            if (TRAPPED.indexOf(k) != -1)
+            if (TRAPPED.indexOf(k) !== -1) {
                 e.preventDefault();
+            }
         }
 
-        function init () {
+        function init() {
             $(document).on('keypress', keypress);
         }
 
         function appendImage(galleryIndex, imageIndex, image) {
-            if (!state.galleries[galleryIndex])
-                state.galleries[galleryIndex] = []
+            if (!state.galleries[galleryIndex]) {
+                state.galleries[galleryIndex] = [];
+            }
             state.galleries[galleryIndex].push(image);
 
             image.addClass('shmui-item');
@@ -418,14 +431,13 @@
         return this.each(function (i) {
             var el = $(this);
 
-            if (el[0].tagName == 'IMG') {
-                appendImage(0, i, el)
+            if (el[0].tagName === 'IMG') {
+                appendImage(0, i, el);
             } else {
-                el.find('img').each(function (j) { 
+                el.find('img').each(function (j) {
                     appendImage(i, j, $(this));
                 });
             }
         });
     };
 }) (jQuery);
-
